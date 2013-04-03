c5ebd264f63b4546cd2e1b4fd8284d4b0d8fc5c9
Sébastien Saunier <sebastien.saunier@gmail.com>
2013-04-03 09:54:01 +0200

    Get password from keychain for easier deployment

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-echo "cd www\nmput .htaccess\nmput  *\nmkd img\ncd img\nlcd img\nmput *" | ftp -i ftp://takone@ftp.takone.fr
+HOST="ftp.takone.fr"
+USER=takone
+PASSWORD=`security find-internet-password -l "$HOST" -g 2>&1 | grep "password" | cut -d \\" -f 2`
+echo "cd www\nmput .htaccess\nmput  *\nmkd img\ncd img\nlcd img\nmput *" | ftp -i ftp://$USER:$PASSWORD@$HOST
